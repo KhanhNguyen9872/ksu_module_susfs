657b51caded1e8f4d544d0dcccb56bf38ebd222c
Nguyễn Văn Khánh <88880309+KhanhNguyen9872@users.noreply.github.com>
2024-11-08 00:15:26 +0700

    add more sus_path for hide custom rom

--- post-fs-data.sh
+++ post-fs-data.sh
@@ -10,6 +10,35 @@ source ${MODDIR}/utils.sh
 ${SUSFS_BIN} add_sus_path /system/addon.d
 ${SUSFS_BIN} add_sus_path /vendor/bin/install-recovery.sh
 ${SUSFS_BIN} add_sus_path /system/bin/install-recovery.sh
+${SUSFS_BIN} add_sus_path /system/vendor/bin/install-recovery.sh
+${SUSFS_BIN} add_sus_path /system/etc/permissions/privapp-permissions-com.crdroid.faceunlock.xml
+${SUSFS_BIN} add_sus_path /system/etc/sysconfig/hiddenapi-whitelist-com.crdroid.faceunlock.xml
+${SUSFS_BIN} add_sus_path /system_ext/etc/permissions/privapp_whitelist_com.crdroid.updater.xml
+${SUSFS_BIN} add_sus_path /system_ext/etc/init/init.crdroid-system_ext.rc/system/framework/org.lineageos.platform-res.apk
+${SUSFS_BIN} add_sus_path /system/framework/oat/arm64/org.lineageos.platform.vdex
+${SUSFS_BIN} add_sus_path /system/framework/oat/arm64/org.lineageos.platform.odex
+${SUSFS_BIN} add_sus_path /system/framework/org.lineageos.platform-res.apk
+${SUSFS_BIN} add_sus_path /system/framework/oat/arm64/org.lineageos.platform.vdex
+${SUSFS_BIN} add_sus_path /system/framework/oat/arm64/org.lineageos.platform.odex
+${SUSFS_BIN} add_sus_path /system/framework/org.lineageos.platform.jar
+${SUSFS_BIN} add_sus_path /system/addon.d/50-lineage.sh
+${SUSFS_BIN} add_sus_path /product/etc/permissions/org.lineageos.settings.xml
+${SUSFS_BIN} add_sus_path /product/etc/permissions/org.lineageos.trust.xml
+${SUSFS_BIN} add_sus_path /product/etc/permissions/org.lineageos.profiles.xml
+${SUSFS_BIN} add_sus_path /product/etc/permissions/org.lineageos.android.xml
+${SUSFS_BIN} add_sus_path /product/etc/permissions/org.lineageos.hardware.xml
+${SUSFS_BIN} add_sus_path /product/etc/permissions/org.lineageos.globalactions.xml
+${SUSFS_BIN} add_sus_path /product/etc/permissions/privapp_whitelist_org.lineageos.eleven.xml
+${SUSFS_BIN} add_sus_path /product/etc/permissions/org.lineageos.livedisplay.xml
+${SUSFS_BIN} add_sus_path /product/etc/sysconfig/lineage-sysconfig.xml
+${SUSFS_BIN} add_sus_path /product/etc/sysconfig/org.lineageos.etar.allowlist.xml
+${SUSFS_BIN} add_sus_path /product/overlay/org.lineageos.platform-res__auto_generated_rro_product.apk
+${SUSFS_BIN} add_sus_path /system_ext/etc/permissions/privapp_whitelist_org.lineageos.lineageparts.xml
+${SUSFS_BIN} add_sus_path /system_ext/etc/permissions/privapp_whitelist_org.lineageos.audiofx.xml
+${SUSFS_BIN} add_sus_path /system_ext/etc/permissions/privapp_whitelist_org.lineageos.lineagesettings.xml
+${SUSFS_BIN} add_sus_path /system_ext/etc/permissions/privapp_whitelist_org.lineageos.setupwizard.xml
+${SUSFS_BIN} add_sus_path /system_ext/etc/init/init.lineage-system_ext.rc
+${SUSFS_BIN} add_sus_path /system_ext/etc/init/init.lineage-updater.rc
 
 #KernelSU
 ${SUSFS_BIN} add_sus_mount /data/adb/modules
